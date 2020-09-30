792a2f02bd14ae70bf9e1121562f3ee93d422f91
JuliaZamaitat <j.zamaitat@gmail.com>
2020-09-30 19:54:47 +0200

    Seperated Recipes into two componentes.

---
+++ client/src/components/Recipe.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <div class="card small-card shadow-sm">
+        <div class="card-header text-right p-0">
+            <a class="btn delete-button" @click="deleteRecipe">
+            <font-awesome-icon icon="trash" />
+            </a>
+        </div>
+        <div class="card-body pb-0">
+            <div class="recipe-text" contenteditable="true" @blur="updateRecipe" >{{ recipe.title }}</div>
+        </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import AuthService from '@/services/ApiService.js'
+export default {
+  props: ['recipe'],
+  methods: {
+    updateRecipe (e) {
+    //   AuthService.updateNote(this.id, e.target.innerText)
+    },
+    async deleteRecipe () {
+    //   await AuthService.deleteNote(this.id)
+    //   this.$parent.getNotes()
+    }
+  }
+}
+</script>
+
+<style scoped>
+.small-card.card {
+  background-color: rgba(246, 204, 118, 0.35);
+  border: 0;
+  box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.14),
+    0 2px 1px -1px rgba(0, 0, 0, 0.12), 0 1px 3px 0 rgba(0, 0, 0, 0.2) !important;
+}
+</style>
--- client/src/components/Recipes.vue
+++ client/src/components/Recipes.vue
@@ -2,21 +2,25 @@
     <div>
         <h1>Your Recipes</h1>
         <div v-for="recipe in recipes" :key="recipe.id">
-            <h2>{{recipe.title}}</h2>
+             <Recipe :recipe="recipe"></Recipe>
         </div>
     </div>
 </template>
 
 <script>
 import ApiService from '@/services/ApiService.js'
+import Recipe from '@/components/Recipe.vue'
 export default {
+  components: {
+    Recipe
+  },
   data () {
     return {
       recipes: []
     }
   },
   created () {
-      this.getRecipes()
+    this.getRecipes()
   },
   methods: {
     async getRecipes () {
